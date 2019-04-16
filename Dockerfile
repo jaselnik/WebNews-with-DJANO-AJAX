34cd98ef016e363871ad29b100790ecedffa6c45
Denis Tamkovich <tamkovichdenis@gmail.com>
2019-04-16 17:51:45 +0300

    [IMP] added middleware, updated file structure

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM python:3
+
+ARG requirements=requirements/production.txt
+
 RUN mkdir src/
-COPY requirements.txt src/
+COPY requirements/ src/
 WORKDIR /src/
-RUN pip install -r requirements.txt
 ADD . /src/
+RUN pip install -r $requirements
---
+++ ajax_web_news/settings/dev.py
@@ -0,0 +1,25 @@
+import os
+
+from ajax_web_news.settings.base import *
+
+BASE_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), os.pardir
+)
+
+DEBUG = True
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        # 'PASSWORD': 'password',
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static_in_dev"),)
